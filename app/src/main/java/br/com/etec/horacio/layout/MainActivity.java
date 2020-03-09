475c7400f6b852eba968c98f009f2fb101df479a
Diego <diegocaboclo@hotmail.com>
2020-03-09 20:25:42 -0300

    commit inicial

---
+++ app/src/main/java/br/com/etec/horacio/layout/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.etec.horacio.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
